ff1176532a60f832f85860711619d45ba5111019
Pouya Samadnezhad <pouyasamadnejad@gmail.com>
2024-05-13 18:37:14 +0330

    creat react app

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { CORE_CONCEPTS } from "./date.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept/CoreConsept.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              image={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CoreConcept/CoreConsept.jsx
@@ -0,0 +1,10 @@
+import "../CoreConcept/CoreConcept.css";
+export default function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
